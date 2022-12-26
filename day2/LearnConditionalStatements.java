71366cc394e8014ed00db45a445a803544266739
Deepana-S <121450641+Deepana-S@users.noreply.github.com>
2022-12-26 22:50:11 +0530

    Add files via upload
    week1

---
+++ day2/LearnConditionalStatements.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class LearnConditionalStatements {
+
+	public static void main(String[] args) {
+		int age=20;
+		if(age<18) {
+			System.out.println(age+ "is not eligible to vote");
+		}
+		
+		else if(age>18) {
+			System.out.println(age+ "is eligible to vote");
+	}
+
+	}}	
+
+
